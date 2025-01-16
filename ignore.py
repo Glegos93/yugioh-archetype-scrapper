a463a68601ddf3e05eed445bd9bfb0eb9b9c62d1
Glegos93 <50709521+Glegos93@users.noreply.github.com>
2025-01-16 09:28:16 -0600

    feat: filtered out majority of non playables

---
+++ ignore.py
@@ -0,0 +1,22 @@
+ignore_from_current = [
+    "gemini", #typing
+    "illusion", #typing
+    "pendulum magicians",#Request
+    "psychic*", #name
+    "rokket/borrel/topologic", #combination series
+    "silent*", #silent in name only
+    "sinful spoils/ snake-eyes", #needs to be blown up and separated
+    "swordsman(archetype)*", #name
+    ]
+
+ignore_from_archetype = [
+    # Add items here if needed
+]
+
+ignore_from_series = [
+    # Add items here if needed
+]
+
+# To delete in Future
+# sinful spoils/ snake-eyes
+
--- main.py
+++ main.py
@@ -19,23 +19,26 @@ def read_from_file(filename):
 
 # Define the URLs
 # Do not change the following URLs
-archetype_url = 'https://yugipedia.com/wiki/Category:TCG_and_OCG_archetypes'
-series_url = 'https://yugipedia.com/wiki/Category:TCG_and_OCG_series'
+archetype_url = 'https://yugipedia.com/wiki/Category:archetypes'
+series_url = 'https://yugipedia.com/wiki/Category:series'
 current_file = 'current.txt'
+series_url_anime_manga = 'https://yugipedia.com/wiki/Category:Anime_and_manga_only_series'
+series_url_rush_duel = 'https://yugipedia.com/wiki/Category:Rush_Duel_series'
+archetype_url_anime_manga = 'https://yugipedia.com/wiki/Category:Anime_and_manga_only_archetypes'
 
 def display_menu():
     print("Please choose an option:")
     print("1. Archetype (write to file)")
     print("2. Series (write to file)")
     print("3. Both (write to file)")
-    print("4. Archetype (print to console)")
-    print("5. Series (print to console)")
-    print("6. Both (print to console)")
-    print("7. Archetype minus current.txt (print to console)")
-    print("8. Series minus current.txt (print to console)")
-    print("9. Both minus current.txt (print to console)")
-    print("10. Unique to current.txt (print to console)")
-    print("11. Import data from Google Sheets")
+    print("4. Import data from Google Sheets")
+    print("5. Archetype (print to console)")
+    print("6. Series (print to console)")
+    print("7. Both (print to console)")
+    print("8. Archetype minus current.txt (print to console)")
+    print("9. Series minus current.txt (print to console)")
+    print("10. Both minus current.txt (print to console)")
+    print("11. Unique to current.txt (print to console)")
     print("12. Exit")
 
 print("Starting the program...")
@@ -47,13 +50,20 @@ while True:
     if choice == '1':
         print("Scraping archetype data...")
         archetype_titles = scrape_archetype(archetype_url)
-        sorted_titles = sorted(set(archetype_titles))
+        anime_manga_archetype_titles = scrape_archetype(archetype_url_anime_manga)
+        filtered_titles = [title for title in archetype_titles if "(anime)" not in title.lower() and title.lower() not in (title.lower() for title in anime_manga_archetype_titles)]
+        sorted_titles = sorted(set(title.lower() for title in filtered_titles))
         write_to_file('archetype.txt', sorted_titles)
         print("Archetype data written to archetype.txt")
     elif choice == '2':
         print("Scraping series data...")
         series_titles = scrape_series(series_url)
-        sorted_titles = sorted(set(series_titles))
+        anime_manga_titles = scrape_series(series_url_anime_manga)
+        rush_duel_titles = scrape_series(series_url_rush_duel)
+        filtered_titles = [title for title in series_titles if "(rush duel)" not in title.lower() and "(dm series)" not in title.lower()]
+        filtered_titles = [title for title in filtered_titles if title.lower() not in (title.lower() for title in anime_manga_titles)]
+        filtered_titles = [title for title in filtered_titles if title.lower() not in (title.lower() for title in rush_duel_titles)]
+        sorted_titles = sorted(set(title.lower() for title in filtered_titles))
         write_to_file('series.txt', sorted_titles)
         print("Series data written to series.txt")
     elif choice == '3':
@@ -65,20 +75,31 @@ while True:
         write_to_file('both.txt', sorted_titles)
         print("Combined data written to both.txt")
     elif choice == '4':
+        print("Importing data from Google Sheets...")
+        spreadsheet_name = 'Archetype Draft'
+        sheet_name = 'Data'
+        range_name = 'A2:A455'  # Adjust the range as needed
+        data = get_google_sheets_data(spreadsheet_name, sheet_name, range_name)
+        values_array = [row[0].lower() for row in data if row and row[0].strip()]  # Filter out empty rows and cells and convert to lowercase
+        write_to_file(current_file, values_array)  # Write the values to current.txt
+        for value in values_array:
+            print(value)
+        print(f"Total count: {len(values_array)}")
+    elif choice == '5':
         print("Reading archetype data from file...")
         archetype_titles = read_from_file('archetype.txt')
         sorted_titles = sorted(set(archetype_titles))
         for title in sorted_titles:
             print(title)
         print(f"Total count: {len(sorted_titles)}")
-    elif choice == '5':
+    elif choice == '6':
         print("Reading series data from file...")
         series_titles = read_from_file('series.txt')
         sorted_titles = sorted(set(series_titles))
         for title in sorted_titles:
             print(title)
         print(f"Total count: {len(sorted_titles)}")
-    elif choice == '6':
+    elif choice == '7':
         print("Reading both archetype and series data from files...")
         archetype_titles = read_from_file('archetype.txt')
         series_titles = read_from_file('series.txt')
@@ -87,7 +108,7 @@ while True:
         for title in sorted_titles:
             print(title)
         print(f"Total count: {len(sorted_titles)}")
-    elif choice == '7':
+    elif choice == '8':
         print("Reading archetype data from file and comparing with current.txt...")
         archetype_titles = read_from_file('archetype.txt')
         current_titles = read_current_file(current_file)
@@ -95,7 +116,7 @@ while True:
         for title in remaining_titles:
             print(title)
         print(f"Total count: {len(remaining_titles)}")
-    elif choice == '8':
+    elif choice == '9':
         print("Reading series data from file and comparing with current.txt...")
         series_titles = read_from_file('series.txt')
         current_titles = read_current_file(current_file)
@@ -103,7 +124,7 @@ while True:
         for title in remaining_titles:
             print(title)
         print(f"Total count: {len(remaining_titles)}")
-    elif choice == '9':
+    elif choice == '10':
         print("Reading both archetype and series data from files and comparing with current.txt...")
         archetype_titles = read_from_file('archetype.txt')
         series_titles = read_from_file('series.txt')
@@ -113,7 +134,7 @@ while True:
         for title in remaining_titles:
             print(title)
         print(f"Total count: {len(remaining_titles)}")
-    elif choice == '10':
+    elif choice == '11':
         print("Reading both archetype and series data from files and finding unique titles in current.txt...")
         archetype_titles = read_from_file('archetype.txt')
         series_titles = read_from_file('series.txt')
@@ -123,17 +144,6 @@ while True:
         for title in unique_titles:
             print(title)
         print(f"Total count: {len(unique_titles)}")
-    elif choice == '11':
-        print("Importing data from Google Sheets...")
-        spreadsheet_name = 'Archetype Draft'
-        sheet_name = 'Data'
-        range_name = 'A2:A455'  # Adjust the range as needed
-        data = get_google_sheets_data(spreadsheet_name, sheet_name, range_name)
-        values_array = [row[0] for row in data if row and row[0].strip()]  # Filter out empty rows and cells
-        write_to_file(current_file, values_array)  # Write the values to current.txt
-        for value in values_array:
-            print(value)
-        print(f"Total count: {len(values_array)}")
     elif choice == '12':
         print("Exiting the program.")
         break
